56df70f3d155ec0397e0d7c38b17f5a498bc3d7a
Manideep Innamuri <deepu.innamuri@gmail.com>
2023-05-19 13:59:15 +0530

    #Connection String,setup of connection string in Program.cs and Product EntityCreation

---
+++ ShoppingApplication/ShoppingApplication.Entities/Data/ShoppingContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShoppingApplication.Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingApplication.Entities.Data
+{
+    public class ShoppingContext : DbContext
+    {
+        public ShoppingContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
